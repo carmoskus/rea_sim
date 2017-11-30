cc7d4d814dda0354c64140fc0a8a016d536c3fca
Chris Armoskus <carmoskus@gmail.com>
2017-11-30 12:57:07 -0800

    Minor edit to usage message

--- sim1_deseq2_notrim.R
+++ sim1_deseq2_notrim.R
@@ -3,7 +3,7 @@ arg.dir = args[1]
 arg.num = args[2]
 
 if (is.na(arg.dir) || is.na(arg.num) || nchar(arg.dir) == 0 || nchar(arg.num) == 0) {
-    write("Usage: sim1_create.R subdir num\n", stderr())
+    write("Usage: sim1_create.R subdir num", stderr())
     quit(save="no", status=1)
 }
 

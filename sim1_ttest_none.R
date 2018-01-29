28360d31cd10853d5d69858a488c1925502f48c6
Chris Armoskus <carmoskus@gmail.com>
2018-01-28 23:56:13 -0800

    Think ttest_none works

--- sim1_ttest_none.R
+++ sim1_ttest_none.R
@@ -9,16 +9,16 @@ if (is.na(arg.dir) || is.na(arg.num) || nchar(arg.dir) == 0 || nchar(arg.num) ==
 
 subdir = paste0("sims/", arg.dir, "/", arg.num, "/")
 
-name = "ttest"
+name = "ttest_none"
 
 counts = as.matrix(read.table(paste0(subdir, "counts.txt"), header=TRUE, sep="\t", row.names=1))
 
 col.info = read.table(paste0(subdir, "cols.txt"), header=TRUE, row.names=1, sep="\t")
 
 # Normalize to total reads
-num.reads = colSums(counts)
-sfs = num.reads / mean(num.reads)
-counts = t(t(counts) / sfs)
+#num.reads = colSums(counts)
+#sfs = num.reads / mean(num.reads)
+#counts = t(t(counts) / sfs)
 
 # Do t-tests
 test = function (row) {

512242181dca9103b3fc3936cadb765d732310fc
Chris Armoskus <carmoskus@gmail.com>
2019-03-19 22:16:13 -0700

    Mostly working, could possibly use something added to it. Not tested.

--- posthoc1aG.R
+++ posthoc1aG.R
@@ -11,30 +11,16 @@ subdir = paste0(root.dir, "/", arg.dir, "/")
 
 name = "ph1a"
 
-conf.file = paste0(root.dir, "/", arg.dir, "/meta.txt")
-if (!file.exists(conf.file)) {
-    write(paste0("Error: no configuration file found at '", conf.file, "'"), stderr())
-    quit(save="no", status=1)
-}
-
-conf.data = read.table(conf.file, sep="\t", stringsAsFactors=FALSE, row.names=1)
-conf = as.list(conf.data$V2)
-names(conf) = rownames(conf.data)
-
-col.info = read.table(paste0(subdir, "cols.txt"), header=TRUE, row.names=1, sep="\t")
-a.names = rownames(col.info)[col.info$group == "a"]
-b.names = rownames(col.info)[col.info$group == "b"]
-
 ## The number of simulations with permutations run on them
 N = 100
 ## "deseq2_notrim" "edgeR" "voom_TMM" "voom" "ttest_log_TMM" "ttest_log"
 analyses = c("deseq2_notrim", "edgeR", "voom_TMM", "voom", "ttest_log_TMM", "ttest_log")
 
 summarize = function (arg.num) {
-    function (analysis) {
-        
-    }
+    ## Load data for this simulation
+    d = read.table(paste0(subdir, arg.num, "/", name, "F.txt"), header=TRUE, sep="\t")
+    colMeans(d)
 }
 
-x = t(sapply(sapply(1:N, summarize), function (f) sapply(analyses, f)))
+x = t(sapply(1:N, summarize))
 write.table(x, file=paste0(subdir, name, "G.txt"), sep="\t", row.names=FALSE, quote=FALSE)

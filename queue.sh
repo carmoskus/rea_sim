99b65d51de4a4bee290114db15fc6801d41e7d1e
Chris Armoskus <carmoskus@gmail.com>
2019-03-25 10:31:05 +0000

    Think queue.sh is working

--- queue.sh
+++ queue.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-i=1
+cmds="commands.txt"
 
-while true
+i=1
+n=`wc -l $cmds | awk '{print $1}'`
+
+while (( $i <= $n ))
 do
     nrun=`jobs|wc -l`
     if (( $nrun < 16 ))
     then
 	echo 'Running line' $i
+	eval `head -n $i $cmds | tail -n 1` &
+	(( i = $i + 1 ))
     else
 	sleep 5
     fi
+#    sleep 1
 done

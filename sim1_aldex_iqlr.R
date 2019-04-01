56b77f1b9ddea0a6e04360fd472d5cb7549ffb8c
Chris Armoskus <carmoskus@gmail.com>
2019-04-01 19:34:08 +0000

    More changes consolidating aldex tests

--- sim1_aldex_iqlr.R
+++ sim1_aldex_iqlr.R
@@ -10,9 +10,7 @@ if (is.na(arg.dir) || is.na(arg.num) || nchar(arg.dir) == 0 || nchar(arg.num) ==
 subdir = paste0("sims/", arg.dir, "/", arg.num, "/")
 
 denom = "iqlr"
-test = 2 # 1 for Welch's t-test; 2 for Wilcoxon test
-
-name = paste0("aldex_", denom, "_", test)
+##test = 1 # 1 for Welch's t-test; 2 for Wilcoxon test
 
 countsN = as.matrix(read.table(paste0(subdir, "counts.txt"), header=TRUE, sep="\t", row.names=1))
 
@@ -34,27 +32,32 @@ library(ALDEx2)
 ald = aldex(counts, as.character(col.info$group), denom=denom)
 
 ## Make output data frame
-if (test == 1) {
-    p = ald$we.ep
-} else {
-    p = ald$wi.ep
+mk.out = function (test) {
+    if (test == 1) {
+        p = ald$we.ep
+    } else {
+        p = ald$wi.ep
+    }
+
+    df = data.frame(rowMeans(counts[rownames(ald),]), ald$diff.btw, ald$effect, ncol(counts)-2, p)
+    colnames(df) = c("baseMean", "log2FC", "stat", "df", "p.value")
+
+    df = df[order(df$p.value),]
+
+    skipped = setdiff(rownames(counts), rownames(ald))
+    df2 = data.frame(baseMean=rowMeans(counts[skipped,]))
+    df2$log2FC = NA
+    df2$stat = NA
+    df2$df = NA
+    df2$p.value = NA
+
+    df.full = rbind(df, df2)
+
+    name = paste0("aldex_", denom, "_", test)
+    write.csv(df.full, file=paste0(subdir, name, "_res.csv"))
 }
 
-df = data.frame(rowMeans(counts[rownames(ald),]), ald$diff.btw, ald$effect, ncol(counts)-2, p)
-colnames(df) = c("baseMean", "log2FC", "stat", "df", "p.value")
-
-df = df[order(df$p.value),]
-
-skipped = setdiff(rownames(counts), rownames(ald))
-df2 = data.frame(baseMean=rowMeans(counts[skipped,]))
-df2$log2FC = NA
-df2$stat = NA
-df2$df = NA
-df2$p.value = NA
-
-df.full = rbind(df, df2)
-
-write.csv(df.full, file=paste0(subdir, name, "_res.csv"))
+sapply(1:2, mk.out)
 
 #write.csv(log2(nc+1), file=paste0(subdir, name, "_log2counts.csv"))
 #write.table(1/nfs, file=paste0(subdir, name, "_sizes.txt"), sep="\t")

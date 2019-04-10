70b286075e2e136e47eb8c6aa998ce49dc147dd3
Chris Armoskus <carmoskus@gmail.com>
2019-04-10 23:39:07 +0000

    Added check_dist2.R to look at moments of raw counts

--- check_dist2.R
+++ check_dist2.R
@@ -3,19 +3,27 @@ arg.dir = args[1]
 arg.num = args[2]
 
 if (is.na(arg.dir) || is.na(arg.num) || nchar(arg.dir) == 0 || nchar(arg.num) == 0) {
-    write("Usage: prog.R subdir num", stderr())
+    write("Usage: prog.R subdir arg.num", stderr())
     quit(save="no", status=1)
 }
 
-subdir = paste0("sims/", arg.dir, "/", arg.num, "/")
+name="check_dist2"
 
-counts = as.matrix(read.table(paste0(subdir, "counts.txt"), header=TRUE, sep="\t", row.names=1))
+library(DescTools)
 
-col.info = read.table(paste0(subdir, "cols.txt"), header=TRUE, row.names=1, sep="\t")
+check = function (i) {
+    subdir = paste0("sims/", arg.dir, "/", i, "/")
+    counts = as.matrix(read.table(paste0(subdir, "counts.txt"), header=TRUE, sep="\t", row.names=1))
+    
+    means = rowMeans(counts)
+    vars = apply(counts, 1, var)
+    skews = apply(counts, 1, Skew)
+    kurts = apply(counts, 1, Kurt)
 
-means = rowMeans(counts)
-vars = apply(counts, 1, var)
+    out = data.frame(Mean=means, Var=vars, Skew=skews, Kurt=kurts)
+    rownames(out) = rownames(counts)
 
-#summary(log10(means[1:1000]+1))
-summary(log10(vars[1:1000]+1))
-cat("\n")
+    write.table(out, file=paste0(subdir, name, ".txt"), sep="\t", quote=FALSE)
+}
+
+check(arg.num)

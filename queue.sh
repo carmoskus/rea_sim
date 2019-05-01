c758b0b82385e63f1aa50fff212c8abbc426fe5f
Chris Armoskus <carmoskus@gmail.com>
2019-05-01 07:05:29 +0000

    Spead up queue.sh by switch to sed

--- queue.sh
+++ queue.sh
@@ -9,21 +9,27 @@ echo '--- Reading from' $cmds
 
 while (( $i <= $n ))
 do
-    nrun=`jobs|wc -l`
-    if (( $nrun < 96 ))
+    (( nto = 96 - `jobs|wc -l` ))
+    if (( $nto > 0 ))
     then
-	echo '--- Running line' $i of $n '-' $nrun
+#	if (( nto == 1 ))
+#	then
+	    echo '--- Running line' $i of $n '-' $nto
+	    l=`sed -n ${i}p "$cmds"`
+	    echo --- $l
+	    $l &
+	    (( i = $i + 1 ))
+#	fi
+	
 	if (( "$i" % 1024 == 0 ))
 	then
 	    n=`wc -l $cmds | awk '{print $1}'`
 	fi
-	l=`head -n $i $cmds | tail -n 1`
-	echo --- $l
-	$l &
-	(( i = $i + 1 ))
     else
 	sleep 1
     fi
 done
+
+wait
 
 echo '--- Finished'

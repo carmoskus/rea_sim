5bbb4ee00f1bbd05265177e0f37d4a3c9eddfe89
Chris Armoskus <carmoskus@gmail.com>
2019-05-01 08:34:17 +0000

    Think queue.sh should be running multiple lines at once properly now

--- queue.sh
+++ queue.sh
@@ -7,24 +7,47 @@ n=`wc -l $cmds | awk '{print $1}'`
 
 echo '--- Reading from' $cmds
 
+
 while (( $i <= $n ))
 do
-    (( nto = 96 - `jobs|wc -l` ))
+    (( nto = 6 - `jobs|wc -l` ))
     if (( $nto > 0 ))
     then
-#	if (( nto == 1 ))
-#	then
-	    echo '--- Running line' $i of $n '-' $nto
+	if (( nto == 1 ))
+	then
+	    echo '--- Running line' $i of $n
 	    l=`sed -n ${i}p "$cmds"`
 	    echo --- $l
 	    $l &
+	    
+	    if (( "$i" % 1024 == 0 ))
+	    then
+		n=`wc -l $cmds | awk '{print $1}'`
+		echo Reloading $i
+	    fi
+
 	    (( i = $i + 1 ))
-#	fi
-	
-	if (( "$i" % 1024 == 0 ))
-	then
-	    n=`wc -l $cmds | awk '{print $1}'`
+	else
+	    # Run lines $i through $j
+	    (( j = $nto - 1 ))
+	    echo '--- Running' $j lines after $i of $n
+	    code=`sed -n "${i},+${j}p" "$cmds"`
+	    IFS=$'\n'
+	    for l in $code
+	    do
+		IFS=$' \t\n'
+		$l &
+	    done
+
+	    if (( $i % 1024 > ( $i + $j ) % 1024 ))
+	    then
+		n=`wc -l $cmds | awk '{print $1}'`
+		echo Reloading $i
+	    fi
+
+	    (( i = $i + $j + 1 ))
 	fi
+	
     else
 	sleep 1
     fi

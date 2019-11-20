bd8fa5eaf4492bbd3dec89bb646a33115e9a9213
Chris Armoskus <carmoskus@gmail.com>
2019-11-20 13:39:26 -0800

    Setup test_edgeR.R to be sourced and provide all appropriate data

--- test_edgeR.R
+++ test_edgeR.R
@@ -1,14 +1,7 @@
-args = commandArgs(trailingOnly=TRUE)
-arg.dir = args[1]
-arg.num = args[2]
+##
 
-arg.dir = "v8nb2ASHG"
-arg.num = "101"
-
-if (is.na(arg.dir) || is.na(arg.num) || nchar(arg.dir) == 0 || nchar(arg.num) == 0) {
-    write("Usage: prog.R subdir num", stderr())
-    quit(save="no", status=1)
-}
+arg.dir = "nullA_v5nb2"
+arg.num = "1"
 
 subdir = paste0("sims/", arg.dir, "/", arg.num, "/")
 
@@ -18,11 +11,10 @@ counts = as.matrix(read.table(paste0(subdir, "counts.txt"), header=TRUE, sep="\t
 
 col.info = read.table(paste0(subdir, "cols.txt"), header=TRUE, row.names=1, sep="\t")
 
-# Start edgeR
+## Start edgeR
 library("edgeR")
 
-mod = model.matrix(~ col.info$group)
-mod0 = model.matrix(~ 1, data=col.info)
+mod = model.matrix(~ group, data=col.info)
 
 y = DGEList(counts=counts)
 y = calcNormFactors(y)
@@ -30,30 +22,38 @@ y = estimateGLMCommonDisp(y, mod)
 y = estimateGLMTrendedDisp(y, mod)
 y = estimateGLMTagwiseDisp(y, mod)
 fit = glmFit(y, mod)
-fit2 = glmFit(y, mod0)
 
-# Output data
+## DEX gene info
 lrt = glmLRT(fit, coef=2)
+tt = topTags(lrt, n=100000, sort.by="none")$table
+## write.csv(topTags(lrt, n=100000), file=paste0(subdir, name, "_res.csv"))
 
-## Stats
-head(fit2$deviance - fit$deviance)
-head(lrt$table$LR)
+## edgeR normalization functions
+e.cpm = cpm(y)
+e.lcpm = cpm(y, log=TRUE)
 
-##topTags(lrt, n=50)
-write.csv(topTags(lrt, n=100000), file=paste0(subdir, name, "_res.csv"))
-
-#sfs = y$samples$lib.size / mean(y$samples$lib.size)
+## Manually normalize counts
 nfs = y$samples$norm.factors
 eff.lib.sizes = y$samples$lib.size * nfs
 sfs = eff.lib.sizes / mean(eff.lib.sizes)
 names(sfs) = colnames(y)
 nc = t(t(counts) / sfs)
-write.csv(log2(nc+1), file=paste0(subdir, name, "_log2counts.csv"))
+nc.l = log2(nc+1)
+## write.csv(log2(nc+1), file=paste0(subdir, name, "_log2counts.csv"))
 
-# Output metadata
-write.table(sfs, file=paste0(subdir, name, "_sizes.txt"), sep="\t")
+## Metadata
+## write.table(sfs, file=paste0(subdir, name, "_sizes.txt"), sep="\t")
 
 mc = data.frame(AveLogCPM=y$AveLogCPM, trended.dispersion=y$trended.dispersion, tagwise.dispersion=y$tagwise.dispersion)
 rownames(mc) = rownames(y)
-write.table(mc, file=paste0(subdir, name, "_meta.txt"), sep="\t")
+## write.table(mc, file=paste0(subdir, name, "_meta.txt"), sep="\t")
+
+m = matrix(c(rowMeans(nc.l), log2(rowMeans(nc)+1), y$AveLogCPM, rowMeans(e.lcpm), log2(rowMeans(e.cpm)+1), tt$logCPM),
+             nrow=nrow(counts))
+colnames(m) = c("nc.l", "log2.nc", "AveLogCPM", "e.lcpm", "log2.cpm", "logCPM")
+
+m = m[, c("nc.l", "e.lcpm", "log2.nc", "log2.cpm", "AveLogCPM")] ## "logCPM"/tt$logCPM is the exact same as AveLogCPM
+m.e = m[rowMeans(m) > quantile(rowMeans(m), probs=0.75),]
+
+m.ec = cor(m.e)
 
